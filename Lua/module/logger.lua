9002b48057fafa5aa432e0357e70d6c2d279fdd8
zhangao <ouchzsc@gmail.com>
2019-08-30 10:12:44 +0800

    logger dump table

--- Lua/module/logger.lua
+++ Lua/module/logger.lua
@@ -1,11 +1,49 @@
+local print = print
+local tconcat = table.concat
+local tinsert = table.insert
+local srep = string.rep
+local type = type
+local pairs = pairs
+local tostring = tostring
+local next = next
+
 local logger = {}
 
+logger.disabled = {
+    protocol = false,
+    predict_move = true,
+}
+
 function logger.info(text, tag)
+    if logger.disabled[tag] then
+        return
+    end
     print(text, "\n" .. debug.traceback())
 end
 
 function logger.error(text)
     CS.UnityEngine.Debug.LogError(text)
+end
+
+function logger.dump(root)
+    local cache = { [root] = "." }
+    local function _dump(t, space, name)
+        local temp = {}
+        for k, v in pairs(t) do
+            local key = tostring(k)
+            if cache[v] then
+                tinsert(temp, "+" .. key .. " {" .. cache[v] .. "}")
+            elseif type(v) == "table" then
+                local new_key = name .. "." .. key
+                cache[v] = new_key
+                tinsert(temp, "+" .. key .. _dump(v, space .. (next(t, k) and "|" or " ") .. srep(" ", #key), new_key))
+            else
+                tinsert(temp, "+" .. key .. " [" .. tostring(v) .. "]")
+            end
+        end
+        return tconcat(temp, "\n" .. space)
+    end
+    return _dump(root, "", "")
 end
 
 return logger
